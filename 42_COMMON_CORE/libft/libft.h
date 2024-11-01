cac95a80cab9dd2077f47fd0fee79199b6a972a3
burakegekocabay <burakegekocabay@outlook.com>
2024-11-01 21:38:56 +0300

    bonus function prototyps added

--- 42_COMMON_CORE/libft/libft.h
+++ 42_COMMON_CORE/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bkocabay <bkocabay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:44:14 by bkocabay          #+#    #+#             */
-/*   Updated: 2024/10/27 11:47:28 by bkocabay         ###   ########.fr       */
+/*   Updated: 2024/11/01 12:13:30 by bkocabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
@@ -24,6 +30,13 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 char	*ft_itoa(int n);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
