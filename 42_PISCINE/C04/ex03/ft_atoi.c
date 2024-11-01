470f278812336e548d3fdd4efc264a576d9da3e3
burakegekocabay <burakegekocabay@outlook.com>
2024-11-01 21:29:20 +0300

    C04 added

---
+++ 42_PISCINE/C04/ex03/ft_atoi.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkocabay <bkocabay@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 10:28:18 by bkocabay          #+#    #+#             */
+/*   Updated: 2024/08/31 14:11:40 by bkocabay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	check(int result, int sign)
+{
+	if (sign % 2 != 0)
+		return (-result);
+	return (result);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	result = 0;
+	i = 0;
+	sign = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign++;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (check(result, sign));
+}
